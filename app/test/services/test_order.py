37334e81ed92daa8d27a860d25048dbc4615f556
MarcoAguirre <42116904+MarcoAguirre@users.noreply.github.com>
2022-09-07 00:58:56 -0500

    Update variable name to be more descriptive

--- app/test/services/test_order.py
+++ app/test/services/test_order.py
@@ -7,12 +7,12 @@ def test_create_order_service(create_order):
 
 
 def test_check_order_values(detail_created_order):
-    current_order = detail_created_order
-    pytest.assume(type(current_order['client_data']) == dict)
-    pytest.assume(type(current_order['size_id']) == int)
-    returned_order_ids = current_order['ingredients_ids']
+    current_order_detail = detail_created_order
+    pytest.assume(type(current_order_detail['client_data']) == dict)
+    pytest.assume(type(current_order_detail['size_id']) == int)
+    returned_order_ids = current_order_detail['ingredients_ids']
     for ingredient_id in returned_order_ids:
         pytest.assume(type(ingredient_id) == int)
-    returned_order_names = current_order['ingredients_names']
+    returned_order_names = current_order_detail['ingredients_names']
     for ingredient_name in returned_order_names:
         pytest.assume(type(ingredient_name) == str)
