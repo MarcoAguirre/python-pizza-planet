4b3e1f7fa8f10d8c6cd08942672e74751c3c61cb
MarcoAguirre <42116904+MarcoAguirre@users.noreply.github.com>
2022-09-07 00:51:51 -0500

    Update test to check client order values

--- app/test/services/test_order.py
+++ app/test/services/test_order.py
@@ -9,8 +9,9 @@ def test_create_order_service(create_order):
     pytest.assume(created_order.status.startswith('308'))
 
 
-def test_check_order_values(create_order_detail):
-    current_order = create_order_detail
+def test_check_order_values(detail_created_order):
+    current_order = detail_created_order
+    pytest.assume(type(current_order['client_data']) == dict)
     pytest.assume(type(current_order['size_id']) == int)
     returned_order_ids = current_order['ingredients_ids']
     for ingredient_id in returned_order_ids:
