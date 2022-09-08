2a6b7336940491304ae68b92ccadcda18fd9312d
MarcoAguirre <42116904+MarcoAguirre@users.noreply.github.com>
2022-09-08 00:38:19 -0500

    Add all assumptions for all response properties

--- app/test/services/test_order.py
+++ app/test/services/test_order.py
@@ -4,6 +4,15 @@ import pytest
 def test_create_order_service(create_order):
     order = create_order.json
     pytest.assume(create_order.status.startswith('200'))
+    pytest.assume(order['_id'])
+    pytest.assume(order['client_address'])
+    pytest.assume(order['client_dni'])
+    pytest.assume(order['client_name'])
+    pytest.assume(order['client_phone'])
+    pytest.assume(order['date'])
+    pytest.assume(order['detail'])
+    pytest.assume(order['size'])
+    pytest.assume(order['total_price'])
 
 
 def test_get_order_by_id_service(client, create_order, order_uri):
