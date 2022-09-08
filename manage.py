48f68b22bf29a037840f9295d76e6c03918c6ac4
MarcoAguirre <42116904+MarcoAguirre@users.noreply.github.com>
2022-09-08 01:16:13 -0500

    Add beverage model to manage file
    This change will allow the backend create Beverage table

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ from flask_migrate import Migrate
 from app import flask_app
 from app.plugins import db
 # flake8: noqa
-from app.repositories.models import Ingredient, Order, OrderDetail, Size
+from app.repositories.models import Ingredient, Order, OrderDetail, Size, Beverage
 
 
 manager = FlaskGroup(flask_app)
@@ -20,6 +20,7 @@ migrate.init_app(flask_app, db)
 @manager.command('test', with_appcontext=False)
 def test():
     return pytest.main(['-v', './app/test'])
+
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
