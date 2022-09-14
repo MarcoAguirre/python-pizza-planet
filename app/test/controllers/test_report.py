dfc313bd41239e48322d19033d8721f6996ce83e
MarcoAguirre <42116904+MarcoAguirre@users.noreply.github.com>
2022-09-14 00:36:37 -0500

    Create test for report controller

---
+++ app/test/controllers/test_report.py
@@ -0,0 +1,12 @@
+import pytest
+from app.controllers import ReportController
+
+
+def test_create_report(app):
+    created_report, error = ReportController.create(report)
+    pytest.assume(error is None)
+    pytest.assume(created_report['customer'])
+    pytest.assume(created_report['ingredient'])
+    pytest.assume(created_report['beverage'])
+    pytest.assume(created_report['size'])
+    pytest.assume(created_report['month'])
