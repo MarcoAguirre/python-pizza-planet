9b7855e839b27e3d6eca14b64f47bc2cb261fde7
MarcoAguirre <42116904+MarcoAguirre@users.noreply.github.com>
2022-09-17 02:51:03 -0500

    Add order seeders to create fake orders

--- app/seeds/db_filler.py
+++ app/seeds/db_filler.py
@@ -1,17 +1,7 @@
-from tkinter.font import names
+from app.repositories.models import IngredientDetail, Size, Ingredient, Beverage, Order
 from flask_seeder import Seeder, Faker, generator
-from app.repositories.models import Size, Ingredient, Beverage, Order
+from datetime import datetime, timezone
 
-import random
-
-# class Beverage(Beverage):
-#     def generate_ingredients_or_beverages(self, _id=None, name=None, price=None):
-#         self._id = _id
-#         self.name = name
-#         self.price = price
-
-#     def __str__(self):
-#         return "ID=%d, Name=%s, Price=%d" % (self._id, self.name, self.price)
 size_list = [
     {'name': 'slice', 'price': 1.25},
     {'name': 'personal', 'price': 1.25},
@@ -26,10 +16,15 @@ ingredient_list = ['Chicken', 'Cheese', 'Tomato',
 
 total_orders = 100
 
+name_list_path = "./app/seeds/data/names/names.txt"
+
+
+def read_txt_file_with_names_line_by_line(path):
+    with open(path) as name:
+        return name.read().splitlines()
+
 
 class SequenceList(generator.Sequence):
-    # def __init__(self, start=1, end=100, **kwargs): #???????????????????????????
-    #     super().__init__(start, end, **kwargs)
     def __init__(self, list: list):
         super().__init__(start=0, end=len(list))
         self._list = list
@@ -39,6 +34,21 @@ class SequenceList(generator.Sequence):
         self._next += 1
 
         return self._list[value]
+
+
+class CustomName(generator.Generator):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._lines = None
+
+    def generate(self):
+        if self._lines is None:
+            self._lines = read_txt_file_with_names_line_by_line(
+                name_list_path)
+
+        result = self.rnd.choice(self._lines)
+
+        return result
 
 
 class DbSeeder(Seeder):
@@ -63,6 +73,32 @@ class DbSeeder(Seeder):
 
         return fake_sizes
 
+    def generate_orders(self):
+        fake_order = Faker(cls=Order, init={
+            "_id": generator.Sequence(end=total_orders),
+            "client_name": CustomName(),
+            "client_dni": generator.String('[0-9]'),
+            "client_address": generator.String('[a-m]'),
+            "client_phone": generator.String('[0-9]'),
+            # # tHIS LINE WILL NEED ITS OWN GENERATOR DUE TO WITH THIS FORM THE DATE WILL BE ALWAYS THE SAME
+            "date": datetime.now(timezone.utc),
+            "total_price": 1,
+            "size_id": generator.Integer(start=1, end=len(size_list))
+        })
+
+        fake_ingredient_detail = Faker(cls=IngredientDetail, init={
+            "_id": generator.Sequence(end=total_orders),
+            "ingredient_price": 1,
+            "order_id": generator.Integer(start=1, end=total_orders),
+            "ingredient_id": generator.Integer(start=1, end=len(ingredient_list))
+        })
+
+        fake_order_list = [order for order in fake_order.create(total_orders)]
+        fake_ingredient_detail_list = [
+            ingredient_detail for ingredient_detail in fake_ingredient_detail.create(total_orders)]
+
+        return fake_order_list, fake_ingredient_detail_list
+
     def save_data_in_corresponding_db(self, data):
         for db_register in data:
             self.db.session.add(db_register)
@@ -76,18 +112,10 @@ class DbSeeder(Seeder):
 
         sizes = [size for size in self.generate_sizes().create(len(size_list))]
 
+        orders, ingredient_details = self.generate_orders()
+
         self.save_data_in_corresponding_db(sizes)
         self.save_data_in_corresponding_db(ingredients)
+        self.save_data_in_corresponding_db(ingredient_details)
         self.save_data_in_corresponding_db(beverages)
-        # faker = Faker(
-        #     cls=Beverage,
-        #     init={
-        #         "_id": generator.Sequence(),
-        #         "name": generator.Name(),
-        #         "price": generator.Integer(start=1, end=10)
-        #     }
-        # )
-
-        # for user in faker.create(5):
-        #     print("Adding user: %s" % user)
-        #     self.db.session.add(user)
+        self.save_data_in_corresponding_db(orders)
