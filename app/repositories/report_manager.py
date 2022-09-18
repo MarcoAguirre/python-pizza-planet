c210bd3f1a7846355d8ef04ebd33924ee2c4a88c
MarcoAguirre <42116904+MarcoAguirre@users.noreply.github.com>
2022-09-18 02:15:15 -0500

    Add function to get most requested ingredient

--- app/repositories/report_manager.py
+++ app/repositories/report_manager.py
@@ -2,7 +2,7 @@ from typing import Any, List, Optional, Sequence
 
 from .models import BeverageDetail, Ingredient, Order, IngredientDetail, Size, Beverage, db
 
-from sqlalchemy import func
+from sqlalchemy import func, desc
 
 
 class BaseReportManager:
@@ -12,13 +12,25 @@ class BaseReportManager:
 
     @classmethod
     def get_most_requested_ingredients(cls):
-        ingredients_gotten = cls.session.query()
+        ingredients_chosen_count = cls.session.query(func.count(cls.order_detail_model.ingredient_id).label('count'),
+                                                     cls.order_detail_model.ingredient_id).group_by(
+            cls.order_detail_model.ingredient_id).order_by(desc('count')).first()
 
-    @classmethod
+        ingredient = Ingredient.query.get(
+            ingredients_chosen_count.ingredient_id)
+
+        most_requested_ingredient = {
+            'name': ingredient.name,
+            'count': ingredients_chosen_count.count
+        }
+
+        return most_requested_ingredient
+
+    @ classmethod
     def get_month_with_more_revenue(cls):
         months_gotten = cls.session.query()
 
-    @classmethod
+    @ classmethod
     def get_more_loyal_customer(cls):
         loyal_customer = cls.session.query()
 
