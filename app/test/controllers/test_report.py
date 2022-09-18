de67750e0cc325fabfdb96364107b061bff90028
MarcoAguirre <42116904+MarcoAguirre@users.noreply.github.com>
2022-09-18 03:46:32 -0500

    Update create report function for controller testing

--- app/test/controllers/test_report.py
+++ app/test/controllers/test_report.py
@@ -3,7 +3,7 @@ from app.controllers import ReportController
 
 
 def test_create_report(app):
-    created_report, error = ReportController.create(report)
+    created_report, error = ReportController().create_report()
     pytest.assume(error is None)
     pytest.assume(created_report['customer'])
     pytest.assume(created_report['ingredient'])
