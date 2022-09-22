064c6b3c48c9340d358be2e43938cecf534c74bd
MarcoAguirre <42116904+MarcoAguirre@users.noreply.github.com>
2022-09-22 00:08:41 -0500

    Update test for report controller to use the correct function
    The test was receiving a dictionary and an error exception. However, the function to generate the report was only returning a dictionary, so there's no need to have the "error" variable here

--- app/test/controllers/test_report.py
+++ app/test/controllers/test_report.py
@@ -2,11 +2,8 @@ import pytest
 from app.controllers import ReportController
 
 
-def test_create_report(app):
-    created_report, error = ReportController().create_report()
-    pytest.assume(error is None)
+def test_create_report(app, create_orders):
+    created_report = ReportController().create_report()
     pytest.assume(created_report['customer'])
     pytest.assume(created_report['ingredient'])
-    pytest.assume(created_report['beverage'])
-    pytest.assume(created_report['size'])
     pytest.assume(created_report['month'])
