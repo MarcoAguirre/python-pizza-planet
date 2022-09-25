ae01bde8ce56ec0d712f2bfbe2886203e44e1e8e
MarcoAguirre <42116904+MarcoAguirre@users.noreply.github.com>
2022-09-25 18:48:08 -0500

    Create report factory

---
+++ app/repositories/reports/factory.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+from app.repositories.models import Ingredient, Order, IngredientDetail, db
+from .report_manager import IReport, ReportManager
+
+
+class IReportFactory(ABC):
+    @abstractmethod
+    def get_created_report(self) -> IReport:
+        pass
+
+
+class ReportFactory(IReportFactory):
+    def __init__(self):
+        self._order = Order
+        self._ingredient_detail = IngredientDetail
+        self._session = db.session
+        self._ingredient = Ingredient
+
+    def get_created_report(self) -> IReport:
+        return ReportManager(self._session, self._order, self._ingredient_detail, self._ingredient)
