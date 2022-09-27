129b5a200a7d824abaaf8be5bc1b414798d54305
MarcoAguirre <42116904+MarcoAguirre@users.noreply.github.com>
2022-09-26 21:32:50 -0500

    Change name for BaseController to a more accurate name

--- app/services/base.py
+++ app/services/base.py
@@ -1,34 +1,34 @@
 from app.common.http_methods import GET, POST, PUT
 from flask import jsonify, request
 
-from ..controllers import base
+from ..controllers.base import BaseController
 
 
 class BaseService():
 
-    def __init__(cls, entity: base):
-        cls.entity = entity
+    def __init__(cls, base_controller: BaseController):
+        cls.base_controller = base_controller
 
     def create(cls, request):
-        entity, error = cls.entity.create(request.json)
-        response = entity if not error else {'error': error}
+        base_controller, error = cls.base_controller.create(request.json)
+        response = base_controller if not error else {'error': error}
         status_code = 200 if not error else 400
         return jsonify(response), status_code
 
     def update(cls):
-        entity, error = cls.entity.update(request.json)
-        response = entity if not error else {'error': error}
+        base_controller, error = cls.base_controller.update(request.json)
+        response = base_controller if not error else {'error': error}
         status_code = 200 if not error else 400
         return jsonify(response), status_code
 
     def get_by_id(cls, _id: int):
-        entity, error = cls.entity.get_by_id(_id)
-        response = entity if not error else {'error': error}
-        status_code = 200 if entity else 404 if not error else 400
+        base_controller, error = cls.base_controller.get_by_id(_id)
+        response = base_controller if not error else {'error': error}
+        status_code = 200 if base_controller else 404 if not error else 400
         return jsonify(response), status_code
 
     def get_entity_items(cls):
-        entities, error = cls.entity.get_all()
+        entities, error = cls.base_controller.get_all()
         response = entities if not error else {'error': error}
         status_code = 200 if entities else 404 if not error else 400
         return jsonify(response), status_code
