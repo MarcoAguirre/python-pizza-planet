8ddcc8aa23ed05b59245275e29c047a701904b5e
MarcoAguirre <42116904+MarcoAguirre@users.noreply.github.com>
2022-09-26 21:35:16 -0500

    Change cls to self to respect python syntax rules

--- app/services/base.py
+++ app/services/base.py
@@ -6,29 +6,29 @@ from ..controllers.base import BaseController
 
 class BaseService():
 
-    def __init__(cls, base_controller: BaseController):
-        cls.base_controller = base_controller
+    def __init__(self, base_controller: BaseController):
+        self.base_controller = base_controller
 
-    def create(cls, request):
-        base_controller, error = cls.base_controller.create(request.json)
+    def create(self, request):
+        base_controller, error = self.base_controller.create(request.json)
         response = base_controller if not error else {'error': error}
         status_code = 200 if not error else 400
         return jsonify(response), status_code
 
-    def update(cls):
-        base_controller, error = cls.base_controller.update(request.json)
+    def update(self):
+        base_controller, error = self.base_controller.update(request.json)
         response = base_controller if not error else {'error': error}
         status_code = 200 if not error else 400
         return jsonify(response), status_code
 
-    def get_by_id(cls, _id: int):
-        base_controller, error = cls.base_controller.get_by_id(_id)
+    def get_by_id(self, _id: int):
+        base_controller, error = self.base_controller.get_by_id(_id)
         response = base_controller if not error else {'error': error}
         status_code = 200 if base_controller else 404 if not error else 400
         return jsonify(response), status_code
 
-    def get_entity_items(cls):
-        entities, error = cls.base_controller.get_all()
+    def get_entity_items(self):
+        entities, error = self.base_controller.get_all()
         response = entities if not error else {'error': error}
         status_code = 200 if entities else 404 if not error else 400
         return jsonify(response), status_code
