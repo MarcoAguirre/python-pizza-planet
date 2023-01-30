127f24bca1c2d6c8aa81b134c3b4cb22b5e0e63f
MarcoAguirre <42116904+MarcoAguirre@users.noreply.github.com>
2023-01-29 19:36:37 -0500

    Disable debug mode

--- app/settings.py
+++ app/settings.py
@@ -5,6 +5,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 class Config:
-    DEBUG = True
+    DEBUG = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pizza.sqlite'))
